ac77bda9f797327231d321464f359c9bc8f68e8e
Ben Carey <bdmc@sinemacula.co.uk>
2026-04-04 11:27:11 -0400

    chore: use centralised coding standards from sinemacula/coding-standards
    Replace inline PHP CS Fixer rules with class-based config from the
    sinemacula/coding-standards Composer package. Includes deliberate
    concat_space violation in helpers.php to verify Qlty Cloud detection.

--- .qlty/configs/.php-cs-fixer.dist.php
+++ .qlty/configs/.php-cs-fixer.dist.php
@@ -1,24 +1,8 @@
 <?php
 
-use PhpCsFixer\Config;
-use PhpCsFixer\Finder;
-use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use SineMacula\CodingStandards\PhpCsFixerConfig;
 
-$vendorDir = getenv('COMPOSER_VENDOR_DIR') ?: dirname(__DIR__, 2) . '/vendor';
-$rules     = require $vendorDir . '/sinemacula/coding-standards/php/.php-cs-fixer.rules.php';
-
-$finder = Finder::create()
-    ->in([
-        dirname(__DIR__, 2) . '/src',
-        dirname(__DIR__, 2) . '/tests',
-    ])
-    ->name('*.php')
-    ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
-
-return (new Config)
-    ->setFinder($finder)
-    ->setUsingCache(true)
-    ->setRiskyAllowed(true)
-    ->setParallelConfig(ParallelConfigFactory::detect())
-    ->setRules($rules);
+return PhpCsFixerConfig::make([
+    dirname(__DIR__, 2) . '/src',
+    dirname(__DIR__, 2) . '/tests',
+]);
